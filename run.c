9603f16239ac2270b9efbd53ddf0931a061919c0
dp3415 <113392305+dp3415@users.noreply.github.com>
2022-12-06 19:41:12 -0800

    added part  1 file

---
+++ run.c
@@ -0,0 +1,59 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+unsigned int xorbuf(unsigned int *buffer, int size) {
+    unsigned int result = 0;
+    for (int i = 0; i < size; i++) {
+        result ^= buffer[i];
+    }
+    return result;
+}
+void read_file(char *filename, int block_size, int block_count){
+	unsigned int buf[block_size];
+	unsigned int xor = 0;
+	int fd  = open(filename,O_RDONLY);
+	for (int i =0; i < block_count; i++){
+		int r = read(fd,buf,block_size);
+		if (r < 0){
+			break;
+		}
+		xor ^= xorbuf(buf,block_size/4);// /4 because int
+	}
+	printf("%08x\n", xor);
+	close(fd);
+}
+
+void write_file(char *filename, int block_size, int block_count){
+	char buf[block_size];
+	int fd  = open(filename,O_WRONLY | O_CREAT);
+	for (int i =0; i < block_count; i++){
+		write(fd,buf,block_size);
+	}
+	close(fd);
+}
+
+int main (int argc,char *argv[]) {
+   if (argc != 5){
+   	printf("incorrect number of inputs please format in ./run <filename> [-r|-w] <block_size> <block_count>");
+   	return 0;
+   }
+   char* filename = argv[1];
+   char flag = argv[2][1];
+   int block_size = atoi(argv[3]);
+   int block_count = atoi(argv[4]);
+   if (flag == 'r'){
+   	read_file(filename,block_size,block_count);
+   }
+   if (flag == 'w'){
+   	write_file(filename,block_size,block_count);
+   }
+   else{
+   	printf("Invalid flag; please use -w for writing, or -w for writing.");
+   }
+  
+   return 0;
+}
