da58c712fa44ea7317d5be195fc6159930ddfbb5
Hani <hani.m17@iiits.in>
2022-12-10 21:15:06 -0500

    add file doubling

--- run2.c
+++ run2.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/time.h>
+
+double now() {
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
 
 unsigned int xorbuf(unsigned int *buffer, int size) {
     unsigned int result = 0;
@@ -11,29 +18,31 @@ unsigned int xorbuf(unsigned int *buffer, int size) {
     }
     return result;
 }
-unsigned int read_file(char *filename, int block_size){
+void read_file(char *filename, int block_size, unsigned int *out_xor, unsigned int *out_block_count, double *read_time){
 	int buf_size = block_size / 4; // 4 because int
 	unsigned int buf[buf_size];
 	unsigned int xor = 0;
 	int fd  = open(filename,O_RDONLY);
 	if (fd == -1) {
 		printf("Cannot open %s\n", filename);
-		return 0;
+		return;
 	}
 	int block_count = 0;
 	int r;
+	double start = now();
 	while ((r=read(fd,buf,block_size)) > 0){
 		block_count += 1;
 		xor ^= xorbuf(buf, buf_size);
 	}
-	printf("%08x\n", xor);
+	*read_time = now() - start;
+	*out_block_count = block_count;
+	*out_xor = xor;
 	close(fd);
-	return block_size * block_count;
 }
 
-void write_file(char *filename, int block_size, int block_count){
+void write_file(char *filename, unsigned int block_size, unsigned int block_count){
 	char buf[block_size];
-	int fd  = open(filename,O_WRONLY | O_CREAT);
+	int fd  = open(filename,O_WRONLY | O_CREAT, 0644);
 	for (int i =0; i < block_count; i++){
 		write(fd,buf,block_size);
 	}
@@ -46,9 +55,19 @@ int main (int argc,char *argv[]) {
    	return 0;
    }
    char* filename = argv[1];
-   int block_size = atoi(argv[2]);
-   unsigned int file_size = read_file(filename,block_size);
-   printf("File size: %u\n", file_size);
+   unsigned int block_size = atoi(argv[2]);
+   unsigned int out_xor;
+   unsigned int out_block_count;
+   unsigned int out_file_size;
+   double read_time = 0;
+   read_file(filename,block_size,&out_xor,&out_block_count,&read_time);
+   while (read_time < 5){
+	printf("Previous Read time: %f. Doubling file size and trying again\n", read_time);
+	if (out_block_count == 0) out_block_count = 1;
+	write_file(filename,block_size,out_block_count*2);
+	read_file(filename,block_size,&out_xor,&out_block_count,&read_time);
+   }
+   printf("XOR: %u Block Count: %u File size: %u Read speed: %f MiB/s\n", out_xor, out_block_count, out_block_count * block_size, (float) out_block_count * block_size / (read_time * 1000000));
   
    return 0;
 }
